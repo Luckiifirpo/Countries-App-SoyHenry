08c2c6428e8314dfd177fcb38ace42f261cda30b
Luckiifirpo <lucianofirpopp@gmail.com>
2022-12-23 13:22:07 -0300

    changes in validations

--- client/src/components/Form/validations.js
+++ client/src/components/Form/validations.js
@@ -5,20 +5,20 @@ export default function Validate(inputs){
 
     // console.log({inputs: inputs});
 
-    if(!inputs.name) errors.name = "Se requiere un nombre para la actividad"
-        else if(inputs.name.length > 50) errors.name = "El nombre no debe contener más de 50 carácteres"
-        else if(!nameValidation.test(inputs.name)) errors.name = "El nombre no puede contener carácteres especiales"
+    if(!inputs.name) errors.name = "A name is required for the activity"
+        else if(inputs.name.length > 50) errors.name = "Name must contain no more than 50 characters"
+        else if(!nameValidation.test(inputs.name)) errors.name = "Name cannot contain special characters"
 
-    if(inputs.difficulty <= 0) errors.difficulty = "La dificultad debe ser mayor que 0"
-        else if(inputs.difficulty >= 6) errors.difficulty = "La dificultad debe ser menor que 6"
+    if(inputs.difficulty <= 0) errors.difficulty = "The difficulty must be greater than 0"
+        else if(inputs.difficulty >= 6) errors.difficulty = "The difficulty must be less than 6"
 
-    if(inputs.duration <= 0) errors.duration = "La duración no debe ser 0 ni negativa"
-        else if(inputs.duration > 60) errors.duration = "La duración no puede ser mayor que 60"
+    if(inputs.duration <= 0) errors.duration = "The duration must not be 0 or negative"
+        else if(inputs.duration > 60) errors.duration = "The duration cannot be more than 60"
 
-    if(!inputs.season) errors.season = "Debes seleccionar una estación del año"
+    if(!inputs.season) errors.season = "You must select a season of the year"
 
-    if(!inputs.durationTime) errors.durationTime = "Debes seleccionar una medida de tiempo"
+    if(!inputs.durationTime) errors.durationTime = "You must select a time measurement"
 
-    if(!inputs.countriesID.length) errors.countriesID = "Debes seleccionar al menos 1 país"
+    if(!inputs.countriesID.length) errors.countriesID = "You must select at least 1 country"
     return errors
 }
